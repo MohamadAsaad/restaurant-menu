0dd99dc2f69487a26de15dd31d0bd2149b35c6cd
MohamadAsaad <33940612+MohamadAsaad@users.noreply.github.com>
2025-07-16 19:54:58 +0300

    Update script.js
    Signed-off-by: MohamadAsaad <33940612+MohamadAsaad@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -409,6 +409,20 @@ function setupPageEffects() {
 
     // تهيئة تأثيرات WOW.js
     new WOW().init();
+
+    // تمكين التمرير السلس لجميع الروابط
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                window.scrollTo({
+                    top: target.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 }
 
 function showToast(message, type = 'success') {
